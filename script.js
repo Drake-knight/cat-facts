088774197b6b13770ac50574383b61e82ab450a9
vishal <jakharvishalv@gmail.com>
2023-06-20 15:05:40 +0530

    Corrections

--- script.js
+++ script.js
@@ -2,30 +2,41 @@
     let factArea = document.getElementById('fact-container');
     let removeButton = document.getElementById('remove-button');
 
-    const getFact = async ()=>{
+      const getFact = async ()=>{
 
-        const response = await fetch('https://cat-fact.herokuapp.com/facts/random');
+        try{
+        const response = await fetch('https://cat-fact.herokuapp.com/facts/rando');
 
         if (response.status !== 200) {
             throw new Error('cannot fetch data');
         }
         const data = await response.json();
-         return data;
-      };
-
-    buttonClick.addEventListener('click', ()=>{
-        getFact().then(data =>{
-            factArea.textContent = data.text;
-        }).catch(err=>{
+        factArea.textContent = data.text;
+         }
+        catch(err){
             factArea.textContent = err.message;
-        })
-            
-        
-    });
+        }}
+      
+
+    buttonClick.addEventListener('click',getFact);
+
+
 
     
-
     const clearBox = ()=>{
         factArea.textContent = '';
     }
     removeButton.addEventListener('click',clearBox);
+
+        
+            
+
+
+
+
+
+
+
+
+
+
